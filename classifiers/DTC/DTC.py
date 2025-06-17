d366b77eb507d724c2fa501c981c783a416a9721
Natalia Owsiejko <n.owsiejko.147@studms.ug.edu.pl>
2025-06-17 13:13:02 +0200

    finished documentation

--- classifiers/DTC/DTC.py
+++ classifiers/DTC/DTC.py
@@ -3,11 +3,18 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+from sklearn.tree import export_graphviz
+import graphviz
+import os
 
 df = pd.read_csv("../../data_sources/gestures.csv")
 
 X = df.drop("label", axis=1).values
 y = df["label"].values
+
+
+feature_names = [f'x{i}' for i in range(21)] + [f'y{i}' for i in range(21)]
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
@@ -23,11 +30,30 @@ cm = confusion_matrix(y_test, clf.predict(X_test), labels=clf.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
 disp.plot(cmap='Blues', xticks_rotation='vertical')
 plt.title("Macierz pomyłek DTC")
-plt.savefig("dtc_extended_confusion_matrix.png")
+plt.savefig("delete.png")
 plt.show()
 
 
-with open('../../models/DTC/DTC_extended.pkl', 'wb') as f:
+try:
+    dot_data = export_graphviz(clf,
+                              out_file=None,
+                              feature_names=feature_names,
+                              class_names=clf.classes_,
+                              filled=True,
+                              rounded=True,
+                              special_characters=True,
+                              max_depth=4)
+    
+    graph = graphviz.Source(dot_data)
+    graph.render("decision_tree_graphviz", format='png', cleanup=True)
+    print("Graphviz tree saved as 'decision_tree_graphviz.png'")
+except:
+    print("Graphviz not available - skipping advanced visualization")
+
+
+
+
+with open('../../models/DTC/delete.pkl', 'wb') as f:
     import pickle
     pickle.dump(clf, f)
 print("Model zapisany do 'DTC.pkl'")
